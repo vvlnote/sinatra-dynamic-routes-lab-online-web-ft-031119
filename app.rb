2b894550dd4141858b397a94fec7131115aff2e2
Joy Lin <vvlnote@hotmail.com>
2019-04-28 23:30:39 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,6 +15,7 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do
     num = params[:number].to_i
     phrase = params[:phrase]
+    "#{num}"
     for i in 1..num do
       "#{phrase}"
     end
