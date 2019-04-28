b62554277c356429728a9697a5654f57cebad3df
Joy Lin <vvlnote@hotmail.com>
2019-04-28 23:51:41 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -29,4 +29,13 @@ class App < Sinatra::Base
     "#{str}"
   end
   
+  get '/:operation/:number1/:number2' do
+    num1 = params[:number1].to_i
+    num2 = params[:number2].to_i
+    
+    "#{num1 + num2}"
+    "#{num1 - num2}"
+    "#{num1 * num2}"
+  end
+  
 end
