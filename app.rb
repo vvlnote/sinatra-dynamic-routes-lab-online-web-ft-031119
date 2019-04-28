4eda403cb29c4aecc3127f7f90298ee1158a8a38
Joy Lin <vvlnote@hotmail.com>
2019-04-28 23:48:52 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -24,7 +24,7 @@ class App < Sinatra::Base
   end
   
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-    str = params[:word1] + ", " + params[:word2] + ", " + params[:word3] + ", " + params[:word4] + ", " + params[:word5]
+    str = params[:word1] + " " + params[:word2] + " " + params[:word3] + " " + params[:word4] + " " + params[:word5] + "."
     
     "#{str}"
   end
