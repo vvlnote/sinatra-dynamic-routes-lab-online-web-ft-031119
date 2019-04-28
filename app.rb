0d822d0c1be45cd87c1c1abaf0d071efbd05bdb2
Joy Lin <vvlnote@hotmail.com>
2019-04-28 23:17:58 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,5 +2,17 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
-
+  get '/reversename/:name' do
+    name = params[:name]
+    "#{name.reverse}"
+  end
+  
+  get '/square/:number' do
+    num = params[:number].to_i
+    "#{num*num}"
+  end
+  
+  get '/say/:number/:phrase' do
+    
+  end
 end
